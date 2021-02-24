d4bba63b3da75556146f2aec8b2b7be874b6ec6b
KBryanRL <KBryanRL.bryan_norck@outlook.com>
2021-02-24 10:52:23 -0600

    carrito

--- php/carrito.php
+++ php/carrito.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
 </head>
+
 <body>
     <header>
         <div class="colornavC">
@@ -25,9 +29,12 @@
         </div>
     </header>
     <section>
-        <div class="compra">
-            
+        <div class="contenedor">
+            <div class="factura">
+                <h2>Factura</h2>
+            </div>
         </div>
     </section>
 </body>
+
 </html>
