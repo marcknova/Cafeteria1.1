a66a9b5c554dbe8661494c4573761792a317b454
marcknova <marck.nova@gmail.com>
2021-02-27 23:55:24 -0600

    commit

--- php/procesos/ControlCarrito.php
+++ php/procesos/ControlCarrito.php
@@ -24,7 +24,7 @@ $objOperacion->setOperador($opcion);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../../css/normalize.css">
-    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/style2.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/ba47731c0d.js" crossorigin="anonymous"></script>
@@ -45,48 +45,86 @@ $objOperacion->setOperador($opcion);
                 </div>
             </div>
         </div>
-    </header>
+        </header>
 
-    <section>
-        <div class="contenedor">
+      <div class="container-fact">
             <div class="factura">
-                <h2>Factura</h2>
+                <h1>Coffe: Xpress</h1>
                 <div class="gridFactura">
-                    <div>
-                        <span>Nombre del producto</span>
-                    </div>
-                    <div>
-                        <span>  <?php
-echo ($objOperacion->Nombre());
-?>
-</span>
-                    </div>
-                    <div>
-                        <span>Cantidad</span>
-                    </div>
-                    <div>
-<span>
-<?php
-echo ($objOperacion->Cantidad());
-?>
-</span>
-                    </div>
-                    <div>
-                        <span>Total : </span>
-                    </div>
-                    <div><span class="total">
-                            <?php
-echo ($objOperacion->Operaciones());
-?>
-                        </span>
-                    </div>
+                  <div class="barcode">
+                    <div class="img">
 
-                </div>
+                      <img src="img/como-preparar-cafe-colombiano-758887.jpg">
+                    
+                    </div>
+                  </div>
+                  <div class="vendedor">
+                    <div class="data">
+                        <div>Vendedor</div>
+                        <div>T.T</div>
+                        <div>EmpCent</div>
+                        <div>Operac</div>
+                        <div>Fecha</div>
+                        <div>Hora</div>
+                        <div>EdPizn</div>
+                        <div>T</div>
+                    </div>
+                    <div class="SeparadorIV"></div>
+                      <div class="data">
+                        <div>64008592</div>
+                        <div>3</div>
+                        <div>001014</div>
+                        <div>752772</div>
+                        <div>28/02/21</div>
+                        <div>11:53</div>                       
+                        <div>0106002</div>
+                        <div>00</div>
+                        </div>
+                      </div>
+                      <div class="codecontrol">
+                      <p><b>Morning</b></p>
+                     <p>Codigo de control : 9015D8F1C2</p>
+                      </div>
+                  </div>
+                  <div class="description">
+                        <div><i>Description</i></div>
+                        <div class="two"><i>Cantidad</i></div>
+                        <div class="three"><i>Importe</i></div>
+                  </div>
+                  <div class="SeparadorIV">
+                  </div>
 
-
+            <div class="cantidadProd">
+                    <div><?php
+                      echo ($objOperacion->Nombre());
+                            ?>
+                              </div>
+                      <div class="two">
+                      <?php
+                      echo ($objOperacion->Cantidad());
+                      ?>
+                      </div>
+                       <div class="three">
+                      <p>$20.00</p>
+                    </div>
+                    <br>
             </div>
-        </div>
-    </section>
-</body>
+
+          <div>
+            <p>Dpto:0091 Ref :091801043</p>
+          </div>
+                <div class="total">
+                  <div>
+                    <h3>Total :</h3>
+                  </div>  
+
+                    <div class="tot">
+                            <?php
+                            echo ($objOperacion->Operaciones());
+                            ?>
+                    </div>
+                </div>              
+      </div>
+      </body>
 
 </html>
