9a8f98b57b94197a4b2a60e782ae3f4616d4e613
marcknova <marck.nova@gmail.com>
2021-03-05 13:32:52 -0600

    commit

--- js/app.js
+++ js/app.js
@@ -8,3 +8,33 @@ $(document).ready(function(){
   })
 });
 
+$(document).ready( function() {
+
+  var names = [ "Jörn Zaefferer", "Scott González", "John Resig" ];
+
+  var accentMap = {
+    "á": "a",
+    "ö": "o"
+  };
+  var normalize = function( term ) {
+    var ret = "";
+    for ( var i = 0; i < term.length; i++ ) {
+      ret += accentMap[ term.charAt(i) ] || term.charAt(i);
+    }
+    return ret;
+  };
+
+  $( "#developer" ).autocomplete({
+    source: function( request, response ) {
+      var matcher = new RegExp( $.ui.autocomplete.escapeRegex( request.term ), "i" );
+      response( $.grep( names, function( value ) {
+        value = value.label || value.value || value;
+        return matcher.test( value ) || matcher.test( normalize( value ) );
+      }) );
+    }
+  });
+} );
+
+$(document).ready(function() {
+  $( "#datepicker" ).datepicker();
+});
