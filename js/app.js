e5ea14839f85092243f16dffb2edc35c949ed7f9
marcknova <marck.nova@gmail.com>
2021-03-06 14:57:39 -0600

    commit

--- js/app.js
+++ js/app.js
@@ -67,3 +67,13 @@ $(document).ready( function() {
 $(document).ready(function() {
   $( "#datepicker" ).datepicker();
 });
+
+$(document).ready( function() {
+  var tooltips = $( "[title]" ).tooltip({
+    position: {
+      my: "left top",
+      at: "right+5 top-5",
+      collision: "none"
+    }
+  });
+} );
