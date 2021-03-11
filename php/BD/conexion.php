c9167befa8d2c2f75060a1a74271855151a46e92
KBryanRL <KBryanRL.bryan_norck@outlook.com>
2021-03-11 12:58:25 -0600

    separacion

--- php/BD/conexion.php
+++ php/BD/conexion.php
@@ -25,18 +25,4 @@
            }
         }
      }
-         $objeConexion = new Conexion();
-         $bd = $objeConexion->connect();
-//          $sql = "INSERT INTO logi(id_Usuario, Usuario, email, contraseña) VALUES(?,?,?,?)";          
-//          $result = $bd->prepare($sql);
-//          $result->execute(['48','marco','marck.nova@gmail.com','hola1234']); 
-        
-         $sql = "SELECT * FROM logi ORDER BY id_Usuario ASC";
-         $result1 = $bd->query($sql);
-         foreach($result1 as $data){
-            echo $data['id_Usuario'];
-            echo $data['Usuario'];
-            echo $data['email'];
-            echo $data['contraseña'];
-         }
 ?>
--- php/procesos/sesion.php
+++ php/procesos/sesion.php
@@ -1,4 +1,5 @@
 <?php
+  include_once "../BD/conexion.php";
 
 $nombre = $_POST['nombre'];
 $contraseña = $_POST['contraseña'];
@@ -10,4 +11,19 @@ if (!empty($nombre) && !empty($contraseña)) {
     }
 }else {
     header('Location: ../sesion.html');
+}
+
+$objeConexion = new Conexion();
+$bd = $objeConexion->connect();
+//          $sql = "INSERT INTO logi(id_Usuario, Usuario, email, contraseña) VALUES(?,?,?,?)";          
+//          $result = $bd->prepare($sql);
+//          $result->execute(['48','marco','marck.nova@gmail.com','hola1234']); 
+
+$sql = "SELECT * FROM logi ORDER BY id_Usuario ASC";
+$result1 = $bd->query($sql);
+foreach($result1 as $data){
+   echo $data['id_Usuario'];
+   echo $data['Usuario'];
+   echo $data['email'];
+   echo $data['contraseña'];
 }
